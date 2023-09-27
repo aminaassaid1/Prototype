b4e1a74d990747d9255966108ee665ddf337a445
aminaassaid1 <assaid.amina.solicode@gmail.com>
2023-09-27 13:54:31 +0100

    add database

--- Prototype2/prototype2.sql
+++ Prototype2/prototype2.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 27, 2023 at 03:28 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 27 sep. 2023 à 12:20
+-- Version du serveur : 10.4.28-MariaDB
+-- Version de PHP : 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,140 +18,102 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `prototype2`
+-- Base de données : `prototype2`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `personne`
+-- Structure de la table `personne`
 --
 
 CREATE TABLE `personne` (
   `id` int(11) NOT NULL,
   `nom` varchar(50) NOT NULL,
-  `prenom` varchar(50) NOT NULL
+  `prenom` varchar(50) NOT NULL,
+  `CNE` varchar(500) NOT NULL,
+  `id_ville` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `personne`
+-- Déchargement des données de la table `personne`
 --
 
-INSERT INTO `personne` (`id`, `nom`, `prenom`) VALUES
-(10, 'nada', 'lanjri riach'),
-(12, 'Ahmed', 'Alami'),
-(15, 'Souhaib', 'Karami'),
-(16, 'Karim', 'Kadiri'),
-(17, 'Hamza', 'Amrani');
+INSERT INTO `personne` (`id`, `nom`, `prenom`, `CNE`, `id_ville`) VALUES
+(1, 'karim', 'alami', '', 4),
+(2, 'Amine', 'Kadiri', '', 1),
+(3, 'Soufiane', 'Ben ali', '', 5),
+(4, 'Souhaib', 'Sabiri', '', 1),
+(5, 'Anas', 'souiri', '', 7),
+(6, 'aymane', 'ajoumi', '', 3);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `stagiaire`
---
-
-CREATE TABLE `stagiaire` (
-  `id` int(11) NOT NULL,
-  `id_personne` int(11) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `stagiaire`
---
-
-INSERT INTO `stagiaire` (`id`, `id_personne`, `email`, `password`) VALUES
-(7, 10, 'nadariachlanjri@gmail.com', '$2y$10$6I80XiZiy..bt9CCCdgW6.qmjP3DfSL2IABcBI6sfcXfX6UP3BJsS'),
-(8, 12, 'ahmed@gmail.com', '$2y$10$d5nkQ22/d.oPWALGsQs5QeAxcuiG0QNeT44eeBWlnFUtP45KUwUYW'),
-(11, 15, 'souhaib@gmail.com', '$2y$10$HrrvtEwxnbFXiUIkbACxdef3N8d4WCDy3U34B30u.zbvfMcxwFCCu'),
-(12, 16, 'karim@gmail.com', '$2y$10$VY1ee8URyWXe58AZ938daOqPXByAxNvpBJRNy2IVTyptrHfMb6GQy'),
-(13, 17, 'hamza@gmail.com', '$2y$10$i4SkcycksZQ4Q6htU5yl1ub87nAdDRGT3yL0fd23bKUw10m6yTJi2');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ville`
+-- Structure de la table `ville`
 --
 
 CREATE TABLE `ville` (
   `id` int(11) NOT NULL,
-  `nom_ville` varchar(50) NOT NULL,
-  `id_personne` int(11) NOT NULL
+  `nom_ville` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `ville`
+-- Déchargement des données de la table `ville`
 --
 
-INSERT INTO `ville` (`id`, `nom_ville`, `id_personne`) VALUES
-(2, 'Tanger', 10),
-(3, 'Casablanca', 12),
-(6, 'Marrakech', 15),
-(7, 'Agadir', 16),
-(8, 'Marrakech', 17);
+INSERT INTO `ville` (`id`, `nom_ville`) VALUES
+(1, 'Tanger'),
+(2, 'Casablanca'),
+(3, 'Rabat'),
+(4, 'Marrakech'),
+(5, 'Agadir'),
+(6, 'Mohammédia'),
+(7, 'Tétouan'),
+(8, 'Fès');
 
 --
--- Indexes for dumped tables
+-- Index pour les tables déchargées
 --
 
 --
--- Indexes for table `personne`
+-- Index pour la table `personne`
 --
 ALTER TABLE `personne`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_ville` (`id_ville`);
+
+--
+-- Index pour la table `ville`
+--
+ALTER TABLE `ville`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `stagiaire`
---
-ALTER TABLE `stagiaire`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_personne` (`id_personne`);
-
---
--- Indexes for table `ville`
---
-ALTER TABLE `ville`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_personne` (`id_personne`);
-
---
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
--- AUTO_INCREMENT for table `personne`
+-- AUTO_INCREMENT pour la table `personne`
 --
 ALTER TABLE `personne`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `stagiaire`
---
-ALTER TABLE `stagiaire`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
-
---
--- AUTO_INCREMENT for table `ville`
+-- AUTO_INCREMENT pour la table `ville`
 --
 ALTER TABLE `ville`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
--- Constraints for dumped tables
+-- Contraintes pour les tables déchargées
 --
 
 --
--- Constraints for table `stagiaire`
+-- Contraintes pour la table `personne`
 --
-ALTER TABLE `stagiaire`
-  ADD CONSTRAINT `stagiaire_ibfk_1` FOREIGN KEY (`id_personne`) REFERENCES `personne` (`id`);
-
---
--- Constraints for table `ville`
---
-ALTER TABLE `ville`
-  ADD CONSTRAINT `ville_ibfk_1` FOREIGN KEY (`id_personne`) REFERENCES `personne` (`id`);
+ALTER TABLE `personne`
+  ADD CONSTRAINT `personne_ibfk_1` FOREIGN KEY (`id_ville`) REFERENCES `ville` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
